7c0872c78abf943c4301ffa5dd68ad2b3f78c757
hanyu <hanyu@xiaoma.cn>
2015-04-02 17:25:48 +0800

    ss

---
+++ kefu/src/com/xiaoma/kefu/dao/CustomerDao.java
@@ -0,0 +1,31 @@
+package com.xiaoma.kefu.dao;
+
+import java.util.List;
+
+import com.xiaoma.kefu.model.Customer;
+
+/**
+ * @author frongji
+ * @time 2015年4月1日下午4:55:41
+ *
+ */
+public interface CustomerDao {
+    
+	/**
+	 * 获取全部数据
+	 * @return
+	 */
+	public Integer getAllCustomerCount();
+
+	public List<Customer> getCustomerOrderById(Integer start, Integer offset);
+      
+	/**
+	 * 添加一条
+	 * @param customer
+	 * @return
+	 */
+	public Integer createNewCustomer(Customer customer);
+	
+	public boolean updateCustomer(Customer customer);
+
+}
