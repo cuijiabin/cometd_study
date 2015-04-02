d2c65ad9eda9d935a29a5319bd8c8557b1b079fc
wangxingfei <wangxingfei@xiaoma.cn>
2015-04-02 09:22:32 +0800

    一些表的实体类

---
+++ kefu/src/com/xiaoma/kefu/model/BusiGroup.java
@@ -0,0 +1,75 @@
+package com.xiaoma.kefu.model;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 业务分组	实体类
+ * *********************************
+* @Description: TODO
+* @author: wangxingfei
+* @createdAt: 2015年4月2日上午9:06:10
+**********************************
+ */
+@Entity
+@Table(name="busi_group")
+public class BusiGroup implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private Integer id;
+	@Column(name="styleId")
+	private Integer styleId;
+	@Column(name="name",length=20)
+	private String name;
+	@Column(name="code")
+	private String code;
+	@Column(name="createDate")
+	private Date createDate;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getStyleId() {
+		return styleId;
+	}
+	public void setStyleId(Integer styleId) {
+		this.styleId = styleId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
+	
+	
+	
+}
