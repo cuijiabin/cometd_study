973b3becd54f448a06632a36950cbf9b564052f9
Fengrongji <fengrongji@xiaoma.cn>
2015-04-08 17:52:40 +0800

    fengrongji 常用语

---
+++ kefu/src/com/xiaoma/kefu/service/MessageTypeService.java
@@ -0,0 +1,33 @@
+package com.xiaoma.kefu.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.xiaoma.kefu.dao.MessageTypeDao;
+import com.xiaoma.kefu.model.Customer;
+import com.xiaoma.kefu.model.MessageType;
+import com.xiaoma.kefu.util.PageBean;
+
+/**
+ * @author frongji
+ * @time 2015年4月8日下午3:36:28
+ *
+ */
+@Service
+public class MessageTypeService {
+	
+	@Autowired
+	private MessageTypeDao messageTypeDaoImpl;
+	
+	
+	  /**
+	    * 在弹出的对话框显示详细信息
+	    */
+	   public MessageType getMessageTypeById(Integer id){
+		   return messageTypeDaoImpl.getMessageTypeById(id);
+	   }
+
+
+}
