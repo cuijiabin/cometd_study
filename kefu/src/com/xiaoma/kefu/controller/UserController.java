769d65067483e07a5fcebb7b4549a28374c741c3
yangxiaofeng <yangxiaofeng@xiaoma.cn>
2015-04-10 19:49:28 +0800

    userController更新

--- kefu/src/com/xiaoma/kefu/controller/UserController.java
+++ kefu/src/com/xiaoma/kefu/controller/UserController.java
@@ -1,23 +1,17 @@
 package com.xiaoma.kefu.controller;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Date;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.log4j.Logger;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -31,11 +25,10 @@ import com.xiaoma.kefu.service.UserService;
 import com.xiaoma.kefu.util.Ajax;
 import com.xiaoma.kefu.util.PageBean;
 import com.xiaoma.kefu.util.StringHelper;
-
 /**
  * 
  * @author yangixaofeng
- * 
+ *
  */
 @Controller
 @RequestMapping(value = "user")
@@ -45,13 +38,13 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-
+	
 	@Autowired
 	private RoleService roleService;
-
+	
 	@Autowired
 	private DepartmentService deptService;
-
+	
 	@Autowired
 	private FunctionService funcService;
 
@@ -62,27 +55,13 @@ public class UserController {
 	 * @param password
 	 * @param session
 	 */
-
+	@RequestMapping(value = "login.action", method = RequestMethod.GET)
 	public String login(HttpSession session, String name, String password,
 			Model model) {
-		model.addAttribute("result", Ajax.JSONResult(0, "添加成功!"));
-		return "resultjson";
-	}
-
-	/**
-	 * User Main
-	 * 
-	 * @param name
-	 * @param password
-	 * @param session
-	 */
-	@RequestMapping(value = "main.action", method = RequestMethod.GET)
-	public String main(HttpSession session, Model model) {
-		List list = funcService.findFuncOne();
-		model.addAttribute("topList", list);
+        List list = funcService.findFuncOne();
+        model.addAttribute("topList", list);
 		return "index";
 	}
-
 	/**
 	 * User login
 	 * 
@@ -92,25 +71,23 @@ public class UserController {
 	 */
 	@RequestMapping(value = "demo.action", method = RequestMethod.GET)
 	public String demo(HttpSession session) {
-
-		// Assert.notNull(name, "userName can not be null!");
-		// Assert.notNull(password, "password can not be null!");
-		// model.addAttribute("msg", "登录名或者密码为空!");
-		// User user = userService.login(name, password);
-		// model.addAttribute("msg", "登录名或者密码不正确!");
-		// if (user != null) {
-		// session.setAttribute("currentUser", user);
-		// return "/views/welcome";
-		// } else {
-		// logger.debug("login failed!username or password is incorrect.");
-		// return "/views/login";
-		// }
+//		Assert.notNull(name, "userName can not be null!");
+//		Assert.notNull(password, "password can not be null!");
+//		model.addAttribute("msg", "登录名或者密码为空!");
+//		User user = userService.login(name, password);
+//		model.addAttribute("msg", "登录名或者密码不正确!");
+//		if (user != null) {
+//			session.setAttribute("currentUser", user);
+//			return "/views/welcome";
+//		} else {
+//			logger.debug("login failed!username or password is incorrect.");
+//			return "/views/login";
+//		}
 		return "demo";
 	}
 
 	/**
 	 * 查询
-	 * 
 	 * @param conditions
 	 * @param pageBean
 	 * @return
@@ -119,21 +96,19 @@ public class UserController {
 	@RequestMapping(value = "find.action", method = RequestMethod.GET)
 	public String queryAll(Model model, String userName, String phone,
 			Integer currentPage, Integer pageRecorders) {
+      try{
+		currentPage = (currentPage == null) ? 1 : currentPage;
+		pageRecorders = (pageRecorders == null) ? 10 : pageRecorders;
+		PageBean<User> pageBean = userService.getResultByuserNameOrPhone(
+				currentPage, pageRecorders, userName, phone);
 
-		try {
-			currentPage = (currentPage == null) ? 1 : currentPage;
-			pageRecorders = (pageRecorders == null) ? 10 : pageRecorders;
-			PageBean<User> pageBean = userService.getResultByuserNameOrPhone(
-					currentPage, pageRecorders, userName, phone);
-
-
-			model.addAttribute("list", pageBean.getObjList());
-			model.addAttribute("pageBean", pageBean);
-			model.addAttribute("phone", phone);
-			model.addAttribute("userName", userName);
-			return "/set/govern/userList";
-		} catch (Exception e) {
-			model.addAttribute("error", "对不起出错了");
+		model.addAttribute("list", pageBean.getObjList());
+		model.addAttribute("pageBean", pageBean);
+		model.addAttribute("phone", phone);
+		model.addAttribute("userName", userName);
+		return "/set/govern/userList";
+      }catch(Exception e){
+			model.addAttribute("error","对不起出错了");
 			return "/views/error500";
 		}
 	}
@@ -145,19 +120,16 @@ public class UserController {
 	@RequestMapping(value = "add.action", method = RequestMethod.GET)
 	public String changadd(Model model, User user) {
 		try {
-
-			List<Role> rlist = (List<Role>) roleService.findRole();
-			List<Department> dlist = deptService.findDept();
-			model.addAttribute("deptList", dlist);
-			model.addAttribute("roleList", rlist);
+			List<Role> rlist= (List<Role>) roleService.findRole();
+			List<Department> dlist= deptService.findDept();
+			model.addAttribute("deptList",dlist);
+			model.addAttribute("roleList",rlist);
 		} catch (Exception e) {
-
-
+			
 		}
 
 		return "/set/govern/addUser";
 	}
-
 	/**
 	 * 添加到数据库
 	 */
@@ -166,11 +138,11 @@ public class UserController {
 	public String addUser(Model model, User user) {
 		try {
 			// 对用户密码进行加密！
-
+			String password = new String(DigestUtils.md5Hex(user.getPassword().getBytes("UTF-8")));
 			user.setPassword(password);
-
-			Integer isSuccess = userService.createNewUser(user);
-
+            
+			Integer isSuccess = userService.createNewUser(user
+					);
 			if (isSuccess!=0) {
 				model.addAttribute("result", Ajax.JSONResult(0, "添加成功!"));
 			} else {
@@ -182,22 +154,20 @@ public class UserController {
 
 		return "resultjson";
 	}
-
-
+	
 	/**
 	 * 查询出角色列表
 	 */
 	@RequestMapping(value = "role.action", method = RequestMethod.GET)
 	public String role(Model model) {
-
-		try {
-			List<Role> list = (List<Role>) roleService.findRole();
-			System.out.println(list);
-			JSONArray json = JSONArray.fromObject(list);
-			model.addAttribute("result", json.toString());
-			return "views/resultjson";
-		} catch (Exception e) {
-			model.addAttribute("error", "对不起出错了");
+      try{
+		List<Role> list= (List<Role>) roleService.findRole();
+		System.out.println(list);
+		JSONArray json = JSONArray.fromObject(list);
+		model.addAttribute("result", json.toString());
+		return "views/resultjson";
+      }catch(Exception e){
+			model.addAttribute("error","对不起出错了");
 			return "/views/error500";
 		}
 	}
@@ -207,35 +177,32 @@ public class UserController {
 	 */
 	@RequestMapping(value = "dept.action", method = RequestMethod.GET)
 	public String findDept(Model model) {
-
-		try {
-			List<Department> list = deptService.findDept();
-			JSONArray json = JSONArray.fromObject(list);
-			model.addAttribute("result", json.toString());
-			System.out.println(json.toString());
-			return "views/resultjson";
-		} catch (Exception e) {
-			model.addAttribute("error", "对不起出错了");
+	 try{	
+		List<Department> list= deptService.findDept();
+		JSONArray json = JSONArray.fromObject(list);
+		model.addAttribute("result", json.toString());
+        System.out.println(json.toString());
+		return "views/resultjson";
+	 }catch(Exception e){
+			model.addAttribute("error","对不起出错了");
 			return "/views/error500";
 		}
 	}
-
 	/**
 	 * 在弹出的对话框中显示详细信息
 	 */
 	@RequestMapping(value = "detail.action", method = RequestMethod.GET)
 	public String userDetail(Model model, Integer id) {
-
-		try {
-			User user = userService.getUserById(id);
-			List<Role> rlist = (List<Role>) roleService.findRole();
-			List<Department> dlist = deptService.findDept();
-			model.addAttribute("deptList", dlist);
-			model.addAttribute("roleList", rlist);
-			model.addAttribute("user", user);
-			return "/set/govern/addUser";
-		} catch (Exception e) {
-			model.addAttribute("error", "对不起出错了");
+       try{
+		User user = userService.getUserById(id);
+		List<Role> rlist= (List<Role>) roleService.findRole();
+		List<Department> dlist= deptService.findDept();
+		model.addAttribute("deptList",dlist);
+		model.addAttribute("roleList",rlist);
+		model.addAttribute("user", user);
+		return "/set/govern/addUser";
+       }catch(Exception e){
+			model.addAttribute("error","对不起出错了");
 			return "/views/error500";
 		}
 	}
@@ -249,9 +216,7 @@ public class UserController {
 	public String updateUser(Model model, User user) {
 
 		try {
-
 			Integer isSuccess = userService.updateUser(user);
-
 
 			if (isSuccess==1) {
 				model.addAttribute("result", Ajax.JSONResult(0, "修改成功!"));
@@ -265,8 +230,7 @@ public class UserController {
 		return "resultjson";
 
 	}
-
-
+    
 	/**
 	 * 员工离职
 	 * 
@@ -274,14 +238,11 @@ public class UserController {
 	 */
 	@RequestMapping(value = "leave.action", method = RequestMethod.GET)
 	public String updateLeave(Model model, String ids) {
-
-		if (ids == null) {
-			ids = "3";
-		}
+            if(ids==null){
+            	ids="3";
+            }
 		try {
-
 			Integer isSuccess = userService.leaveUser(ids);
-
 
 			if (isSuccess==1) {
 				model.addAttribute("result", Ajax.JSONResult(0, "修改成功!"));
@@ -294,24 +255,22 @@ public class UserController {
 
 		return "resultjson";
 	}
-
-
+	
 	/**
 	 * 员工转移部门
 	 * 
 	 * @return
 	 */
 	@RequestMapping(value = "tradept.action", method = RequestMethod.GET)
-
-	public String updatedept(Model model, String ids, Integer deptId) {
-		if (ids == null) {
-			ids = "3";
-		}
-		if (deptId == null) {
-			deptId = 2;
-		}
+	public String updatedept(Model model, String ids,Integer deptId) {
+            if(ids==null){
+            	ids="3";
+            }
+            if(deptId==null){
+            	deptId=2;
+            }
 		try {
-			boolean isSuccess = userService.tradeUser(ids,deptId);
+			Integer isSuccess = userService.tradeUser(ids,deptId);
 
 			if (isSuccess==1) {
 				model.addAttribute("result", Ajax.JSONResult(0, "修改成功!"));
@@ -324,19 +283,28 @@ public class UserController {
 
 		return "resultjson";
 	}
-
-
+	
+	
 	/**
 	 * 删除
 	 */
 	@RequestMapping(value = "delete.action", method = RequestMethod.GET)
 	public String deletUser(Model model, Integer id) {
+      try{
+		Integer isSuccess = userService.deleteUserById(id);
+		String message = "failure";
+		Integer code = -1;
 
+		if (isSuccess==1) {
+			message = "success";
+			code = 200;
+		}
+		model.addAttribute("message", message);
+		model.addAttribute("code", code);
 
-
-			return "/views/message";
-		} catch (Exception e) {
-			model.addAttribute("error", "对不起出错了");
+		return "/views/message";
+      }catch(Exception e){
+			model.addAttribute("error","对不起出错了");
 			return "/views/error500";
 		}
 
@@ -350,13 +318,10 @@ public class UserController {
 	@RequestMapping(value = "repass.action", method = RequestMethod.GET)
 	public String updateUser(Model model, Integer id, String password) {
 		try {
-
-
+            
 			User toUpdateUser = userService.getUserById(id);
-
-
+			toUpdateUser.setPassword(DigestUtils.md5Hex(password.getBytes("UTF-8")));
 			Integer isSuccess = userService.updateUser(toUpdateUser);
-
 
 			if (isSuccess==1) {
 				model.addAttribute("result", Ajax.JSONResult(0, "密码重置成功!"));
@@ -386,20 +351,17 @@ public class UserController {
 	 */
 
 	@RequestMapping(value = "check.action", method = RequestMethod.GET)
+	public String queryByCheck(Model model, User user) throws UnsupportedEncodingException {
 		
 		try {
-
-			if (user == null || (StringHelper.isEmpty(user.getLoginName()))) {
+			if(user == null || (StringHelper.isEmpty(user.getLoginName()))){
 				model.addAttribute("result", Ajax.toJson(1, "缺少参数，请重新提交！"));
 				return "resultjson";
 			}
 			Integer count = userService.checkUser(user);
-
-			if (count != null && count == 0) {
-
+			if(count!=null && count==0){
 				model.addAttribute("result", Ajax.toJson(0, "该工号名可以使用！"));
-			} else {
-
+			}else{
 				model.addAttribute("result", Ajax.toJson(1, "该工号名已存在！"));
 			}
 		} catch (Exception ex) {
@@ -408,13 +370,11 @@ public class UserController {
 		}
 		return "resultjson";
 	}
-
-
-	public String checkMsg(Model model, String ln, Integer uId, String phone,
-			String msg) throws UnsupportedEncodingException {
+	
+	@RequestMapping(value = "checkMsg.action", method = RequestMethod.GET)
+	public String checkMsg(Model model,String ln,Integer uId, String phone,String msg) throws UnsupportedEncodingException {
 		try {
-
-			if (StringHelper.isEmpty(phone) || StringHelper.isEmpty(msg)) {
+			if(StringHelper.isEmpty(phone) || StringHelper.isEmpty(msg)){
 				model.addAttribute("result", Ajax.toJson(1, "缺少参数，请重新提交！"));
 				return "resultjson";
 			}
@@ -425,6 +385,5 @@ public class UserController {
 		}
 		return "resultjson";
 	}
-
-
+	
 }
