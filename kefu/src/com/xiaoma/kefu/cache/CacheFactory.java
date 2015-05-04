d54336423eef666e77eb65281503a21f11e84218
hanyu <hanyu@xiaoma.cn>
2015-05-04 17:48:57 +0800

    用户缓存添加

--- kefu/src/com/xiaoma/kefu/cache/CacheFactory.java
+++ kefu/src/com/xiaoma/kefu/cache/CacheFactory.java
@@ -5,6 +5,7 @@ import com.xiaoma.kefu.service.BusiGroupDetailService;
 import com.xiaoma.kefu.service.FunctionService;
 import com.xiaoma.kefu.service.InviteIconService;
 import com.xiaoma.kefu.service.ServiceIconService;
+import com.xiaoma.kefu.service.UserService;
 import com.xiaoma.kefu.service.WaitListService;
 import com.xiaoma.kefu.util.StringHelper;
 import com.xiaoma.kefu.util.SysConst.DeviceType;
@@ -63,6 +64,10 @@ public class CacheFactory {
 				BusiGroupDetailService busiGroupDetailService = (BusiGroupDetailService)SpringContextUtil
 						.getBean("busiGroupDetailService");
 				obj=busiGroupDetailService.findUserIdsByStyleId((Integer) key);
+			}else if(cacheName.equals(CacheName.SUSER)){
+				UserService userService = (UserService)SpringContextUtil
+						.getBean("userService");
+				obj=userService.getUserById((Integer) key);
 			}
 		}
 		return obj;
