1fc0b02d52bb65c6172fa5314e9fe634bad6189d
wangxingfei <wangxingfei@xiaoma.cn>
2015-05-08 17:17:54 +0800

    session销毁增加用户为空判断

--- kefu/src/com/xiaoma/kefu/common/SessionListener.java
+++ kefu/src/com/xiaoma/kefu/common/SessionListener.java
@@ -11,14 +11,16 @@ public class SessionListener implements HttpSessionListener{
 
 	@Override
 	public void sessionCreated(HttpSessionEvent arg0) {
-		
+//		System.out.println("session created");
 	}
 
 	@Override
 	public void sessionDestroyed(HttpSessionEvent arg0) {
 		User user = (User) arg0.getSession().getAttribute(CacheName.USER);
-		user.setOnLineStatus(2);//设置为离线
-		CacheMan.update(CacheName.SUSER, user.getId(), user);//更新缓存中的用户状态
+		if(user!=null){
+			user.setOnLineStatus(2);//设置为离线
+			CacheMan.update(CacheName.SUSER, user.getId(), user);//更新缓存中的用户状态
+		}
 	}
 	
 }
