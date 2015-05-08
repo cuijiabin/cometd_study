39bade9386f821eb2ae0c6419d35674945b24653
wangxingfei <wangxingfei@xiaoma.cn>
2015-05-08 17:11:31 +0800

    增加session失效设置

---
+++ kefu/src/com/xiaoma/kefu/common/SessionListener.java
@@ -0,0 +1,24 @@
+package com.xiaoma.kefu.common;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import com.xiaoma.kefu.cache.CacheMan;
+import com.xiaoma.kefu.cache.CacheName;
+import com.xiaoma.kefu.model.User;
+
+public class SessionListener implements HttpSessionListener{
+
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0) {
+		
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		User user = (User) arg0.getSession().getAttribute(CacheName.USER);
+		user.setOnLineStatus(2);//设置为离线
+		CacheMan.update(CacheName.SUSER, user.getId(), user);//更新缓存中的用户状态
+	}
+	
+}
